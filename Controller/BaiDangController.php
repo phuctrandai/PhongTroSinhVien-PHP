47d74c7ad0eeb360c4be74343f450c027c668031
phuctrandai <trandaiphuc.1501@gmail.com>
2018-11-16 15:34:55 +0700

    Update BaiDangController.php

--- Controller/BaiDangController.php
+++ Controller/BaiDangController.php
@@ -1,5 +1,28 @@
 <?php
+session_start();
 
+require_once '../Dao/BaiDangDao.php';
 
-?>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    doPost();
+} else if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    doGet();
+}
 
+function doPost() {
+    doGet();
+}
+
+function doGet() {
+    getBaiDang();
+    //
+    header("Location: ../index.php");
+}
+
+function getBaiDang() {
+    $baiDangDao = new BaiDangDao();
+    $listBaiDang = $baiDangDao->getBaiDang();
+
+    $_SESSION['listBaiDang'] = serialize($listBaiDang);
+//    var_dump($_SESSION['listCategory']);
+}
